19694d6e714999c5fe7b6d1ee2cc00e3082af980
Dea Audreyla Hadi <deaudreyla30@gmail.com>
2025-12-24 23:21:57 +0700

    fix: resolve firebase auth dependency and merge gradle conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-
+    alias(libs.plugins.google.services)
     // Add the Google services Gradle plugin
-    id("com.google.gms.google-services")
+//    id("com.google.gms.google-services")
 }
 
 android {
@@ -12,6 +12,7 @@ android {
     compileSdk {
         version = release(36)
     }
+//    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.serona"
@@ -58,8 +59,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.firebase.auth)
-    implementation(libs.androidx.compose.runtime.livedata)
+    implementation("com.google.firebase:firebase-auth")
+    implementation("androidx.compose.runtime:runtime-livedata")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
